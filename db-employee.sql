3ddbc7b3a51623fcf7ffc4b56c80a0d29ffa751b
Mario Beltran <mariobeltran1999@gmail.com>
2022-08-31 19:37:42 -0600

    :sparkles: add database employee

---
+++ db-employee.sql
@@ -0,0 +1,15 @@
+﻿create database [aspnet-Employee];
+
+use [aspnet-Employee];
+
+create table Employee(
+id INT PRIMARY KEY IDENTITY (1,1) NOT NULL,
+code INT NOT NULL, 
+name VARCHAR(80) NOT NULL,
+surname VARCHAR(80) NOT NULL,
+dateBirthday DATETIME NOT NULL,
+address VARCHAR(150) NULL,
+phone INT NULL,
+position VARCHAR(80) NOT NULL,
+salaryBase DECIMAL NOT NULL
+);
